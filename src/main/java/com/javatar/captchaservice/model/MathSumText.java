55357ec4433e71896e5e463e920c6ccaee4b05dd
JavadAbdi <Mirjavadabdi@gmail.com>
2024-04-28 17:30:59 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/model/MathSumText.java
+++ src/main/java/com/javatar/captchaservice/model/MathSumText.java
@@ -3,6 +3,7 @@ package com.javatar.captchaservice.model;
 import nl.captcha.text.producer.TextProducer;
 
 public class MathSumText implements TextProducer {
+    
     private final String text;
     private final int answer;
     public MathSumText() {
