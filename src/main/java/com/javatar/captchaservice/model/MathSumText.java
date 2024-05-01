4e1b8f88680b1adaf6a19c6d7a9e5813288e2c60
JavadAbdi <Mirjavadabdi@gmail.com>
2024-05-02 00:33:46 +0330

    Updated,Chnaged

--- src/main/java/com/javatar/captchaservice/model/MathSumText.java
+++ src/main/java/com/javatar/captchaservice/model/MathSumText.java
@@ -22,7 +22,7 @@ public class MathSumText implements TextProducer {
             this.text = i + " + " + j + " =";
         }
     }
-    
+
     @Override
     public String getText() {
         return text;
