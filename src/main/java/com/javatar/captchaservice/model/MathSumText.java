f32fabdbf0bbb4d019b93d0becaffbbe13ef940f
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-26 21:09:10 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/model/MathSumText.java
+++ src/main/java/com/javatar/captchaservice/model/MathSumText.java
@@ -21,12 +21,13 @@ public class MathSumText implements TextProducer {
             this.text = i + " + " + j + " =";
         }
     }
-    
+
     @Override
     public String getText() {
         return text;
     }
 
+    
     public int getAnswer() {
         return answer;
     }
