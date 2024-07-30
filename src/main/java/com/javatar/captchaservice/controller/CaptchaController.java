5c1b5aa18ceadecc72906b479e638744cb5b9951
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-30 20:56:03 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/controller/CaptchaController.java
+++ src/main/java/com/javatar/captchaservice/controller/CaptchaController.java
@@ -19,8 +19,7 @@ public class CaptchaController {
 
     @Autowired
     private CaptchaService captchaService;
-
-
+    
     @GetMapping(produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<Resource> getCaptchaImage() {
 
