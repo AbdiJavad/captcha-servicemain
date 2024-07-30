0e5aa59f0c67f22943f61cc1a22b10a08231ed15
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-31 00:18:20 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/CaptchaServiceApplication.java
+++ src/main/java/com/javatar/captchaservice/CaptchaServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaptchaServiceApplication {
-
-
+    
     public static void main(String[] args) {
         SpringApplication.run(CaptchaServiceApplication.class, args);
     }
