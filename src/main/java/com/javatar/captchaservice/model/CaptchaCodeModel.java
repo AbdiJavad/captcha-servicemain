e4f9af9cedc7e1cd675ff6dda92d34e500cc2695
JavadAbdi <Mirjavadabdi@gmail.com>
2024-08-04 23:47:22 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/model/CaptchaCodeModel.java
+++ src/main/java/com/javatar/captchaservice/model/CaptchaCodeModel.java
@@ -6,10 +6,10 @@ import java.util.concurrent.TimeUnit;
 
 @Data
 public class CaptchaCodeModel {
-        private String answer;
+    private String answer;
     private Long expireAt;
 
-    
+
     public CaptchaCodeModel(String answer) {
         this.answer = answer;
         this.expireAt = System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(2);
