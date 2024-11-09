d2805c0e6dcaa00e2f4a44613a3907608e62ce61
JavadAbdi <Mirjavadabdi@gmail.com>
2024-11-09 19:34:42 +0330

    Updated,Changed

--- src/main/java/com/javatar/captchaservice/controller/CaptchaController.java
+++ src/main/java/com/javatar/captchaservice/controller/CaptchaController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("captcha")
 public class CaptchaController {
-
     
     @Autowired
     private CaptchaService captchaService;
